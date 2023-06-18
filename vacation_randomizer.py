0d1cb83f5a0a7f2894a49dfe0f5d2f35cb70cee4
Charles Love Winters III <charlie.winters17@gmail.com>
2023-06-18 15:21:12 -0500

    init commit

---
+++ vacation_randomizer.py
@@ -0,0 +1,34 @@
+import random
+
+JAPAN = "Japan"
+SPAIN = "Spain"
+
+count_metrics = {
+	JAPAN: 0,
+	SPAIN: 0,
+}
+
+years_since_japan_founded = 2023 + 660  # Japan was founded in 660 bc
+years_since_spain_founded = 2023 - 1492 # Spain was founded in 1492 
+
+# Create list entries for how ever many years the country has existed
+japan_entries = [JAPAN for i in range(years_since_japan_founded)]  
+spain_entries = [SPAIN for i in range(years_since_spain_founded)]
+
+# Combine the lists and randomize the order
+all_entries = japan_entries + spain_entries
+random.shuffle(all_entries)
+entry_count = len(all_entries)
+
+for attempt in range(entry_count):
+# Choose a random value between one and the length of the combined list
+	random_index = random.randint(0, entry_count - 1)
+	chosen = all_entries[random_index]
+	count_metrics[chosen] = count_metrics[chosen] + 1
+	print(f"{chosen} has been chosen this time at try {attempt}.")
+
+
+print(f"After {entry_count} attempts, the last chosen value was {chosen}.")
+print(f"SCOREBOARD: Japan: {count_metrics[JAPAN]}, Spain: {count_metrics[SPAIN]}")
+
+
